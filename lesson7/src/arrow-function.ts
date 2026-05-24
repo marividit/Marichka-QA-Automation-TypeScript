692f1f8eb063c841bb1e9049896bbfffe9161462
Maria Svietlakova <mariasvietlakova@gmail.com>
2026-05-24 14:58:42 +0300

    fixed homework7

--- lesson7/src/arrow-function.ts
+++ lesson7/src/arrow-function.ts
@@ -1,8 +1,13 @@
-const sum = (array: number[] | string[]): number | string =>
-    (array as number[]).reduce((acc, item) => (acc as number) + (item as number));
+const sum = (array: number[] | string[]): number | string => {
+    if (typeof array[0] === 'number') {
+        return (array as number[]).reduce((acc, item) => acc + item);
+    }
+    return (array as string[]).reduce((acc, item) => acc + item);
+};
 
 const numbers: number[] = [1, 2, 3, 4, 5];
 const strings: string[] = ['Привіт', ' ', 'світ'];
 
 console.log(sum(numbers)); // 15
 console.log(sum(strings)); // Привіт світ
+
